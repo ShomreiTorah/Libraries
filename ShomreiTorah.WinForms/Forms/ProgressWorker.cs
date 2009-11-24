ce7236e002f73d63450f7857bf53749b4803ccc6
Schabse Laks <Schabse@GMail.com>
2009-11-23 22:22:48 -0500

    Return !WasCancelled, concurrency fixes
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 1435]

--- ShomreiTorah.WinForms/Forms/ProgressWorker.cs
+++ ShomreiTorah.WinForms/Forms/ProgressWorker.cs
@@ -14,34 +14,54 @@ namespace ShomreiTorah.WinForms.Forms {
 		///<summary>Executes an operation and displays its progress.</summary>
 		///<param name="method">The method to execute on the background thread.</param>
 		///<param name="cancellable">Indicates the visibilty of the cancel button.</param>
-		public static void Execute(Action<IProgressReporter> method, bool cancellable) { Execute(null, method, cancellable); }
+		///<returns>False if the cancel button was clicked.</returns>
+		public static bool Execute(Action<IProgressReporter> method, bool cancellable) { return Execute(null, method, cancellable); }
 		///<summary>Executes an operation and displays its progress.</summary>
 		///<param name="parent">The form that will own the progress display.  This parameter can be null.</param>
 		///<param name="method">The method to execute on the background thread.</param>
 		///<param name="cancellable">Indicates the visibilty of the cancel button.</param>
+		///<returns>False if the cancel button was clicked.</returns>
 		[SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Cross-thread exception")]
-		public static void Execute(IWin32Window parent, Action<IProgressReporter> method, bool cancellable) {
+		public static bool Execute(IWin32Window parent, Action<IProgressReporter> method, bool cancellable) {
 			if (method == null) throw new ArgumentNullException("method");
 
 			Exception exception = null;
 
+			bool cancelled = false;
 			using (var dialog = new ProgressForm() { CancelState = cancellable ? ButtonMode.Normal : ButtonMode.Hidden }) {
+				object syncer = new object();
+
+				dialog.FadeIn();
 				ThreadPool.QueueUserWorkItem(delegate {
 					try {
-						method((IProgressReporter)dialog);
+						method(dialog);
 					} catch (Exception ex) {
 						exception = ex;
 					} finally {
-						dialog.FadeOut();
+						lock (syncer) {
+							dialog.Finished = true;
+							dialog.FadeOut();
+						}
 					}
+					lock (syncer) {
+						if (!dialog.IsDisposed)
+							dialog.ShowDialog(parent);
+					}
+					cancelled = dialog.WasCanceled;
 				});
-				dialog.FadeIn();
-				dialog.ShowDialog(parent);
 			}
 			if (exception != null)
 				throw new TargetInvocationException(exception);
+			return !cancelled;
 		}
 		class ProgressForm : ProgressDialog, IProgressReporter {
+			public bool Finished { get; set; }
+
+			protected override void OnFadedIn() {
+				base.OnFadedIn();
+				if (Finished) FadeOut();
+			}
+
 			public new string Caption {
 				get { return base.Caption; }
 				set { MyInvoke(() => base.Caption = value); }
