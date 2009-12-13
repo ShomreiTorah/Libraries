2049740c52460d8798c4fa3342f9899a7605866f
Schabse Laks <Schabse@GMail.com>
2009-12-13 13:32:13 -0500

    Static Maps API v2
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 1526]

--- ShomreiTorah.WinForms/Controls/GoogleMapControl.cs
+++ ShomreiTorah.WinForms/Controls/GoogleMapControl.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Net;
 using System.Globalization;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 
 namespace ShomreiTorah.WinForms.Controls {
 	///<summary>A control that displays a Google Map of an address.</summary>
@@ -21,19 +22,15 @@ namespace ShomreiTorah.WinForms.Controls {
 	[DefaultProperty("Address")]
 	public partial class GoogleMapControl : XtraUserControl {
 		const string ApiKey = "ABQIAAAAxElVnCsXmGsU2ZxWYPP3bhQz-N-4Jc1YS__jTpeG4G3_qb4iFxRvPsoEDVZ1mMOP9BvHOw4WIvAz8A";
-		readonly WebClient geoCoder = new WebClient();
 
 		MapType mapType;
 		int zoom = 15;
 		string addressString = "Passaic, NJ";
 
-		string latitude, longitude;
-
 		int updateCount;
 		///<summary>Creates a new GoogleMapsControl.</summary>
 		public GoogleMapControl() {
 			InitializeComponent();
-			geoCoder.DownloadStringCompleted += geoCoder_DownloadStringCompleted;
 			UpdateMap();
 		}
 
@@ -80,8 +77,6 @@ namespace ShomreiTorah.WinForms.Controls {
 		///<summary>Re-enables autmatic downlaod after calling BeginUpdate.</summary>
 		public void EndUpdate() { if (--updateCount == 0 && updateQueued) DoUpdate(); }
 
-		object currentVersion;
-
 		bool updateQueued;
 		///<summary>Downloads the map from the server.</summary>
 		void DoUpdate() {
@@ -96,7 +91,6 @@ namespace ShomreiTorah.WinForms.Controls {
 		///<summary>Downloads the map image from the server.</summary>
 		public void UpdateMap() {
 			updateQueued = false;
-			currentVersion = new object();
 			pictureBox.Cursor = String.IsNullOrEmpty(AddressString) ? null : Cursors.Hand;
 			pictureBox.Show();
 
@@ -105,14 +99,7 @@ namespace ShomreiTorah.WinForms.Controls {
 				return;
 			}
 
-			pictureBox.CancelAsync();
-			geoCoder.CancelAsync();
-			pictureBox.Image = pictureBox.InitialImage;
-
-			var geoCodeUri = AddKey("http://maps.google.com/maps/geo?output=csv&oe=utf8&q=" + Uri.EscapeDataString(addressString));
-			try {
-				geoCoder.DownloadStringAsync(geoCodeUri, currentVersion);
-			} catch (WebException ex) { SetError(ex.Message); }
+			LoadMapImage();
 		}
 
 		static Uri AddKey(string url) {
@@ -126,32 +113,16 @@ namespace ShomreiTorah.WinForms.Controls {
 
 			return builder.Uri;
 		}
-		void geoCoder_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e) {
-			if (e.UserState != currentVersion) return;
-			latitude = longitude = null;
-			if (e.Error != null)
-				SetError(e.Error.Message);
-			else if (string.IsNullOrEmpty(e.Result))
-				SetError("Google didn't reply");
-			else {
-				var parts = e.Result.Split(',');
-				if (parts.Length == 4 && parts[0] == "200") {
-					latitude = parts[2];
-					longitude = parts[3];
 
-					LoadMapImage();
-				} else SetError(e.Result);
-			}
-		}
 		[SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase", Justification = "Lowercase parameter")]
 		void LoadMapImage() {
-			if (latitude == null) return;
+			if (String.IsNullOrEmpty(AddressString)) return;
 			pictureBox.CancelAsync();
 			var mapTypeString = MapType == MapType.Road ? "roadmap" : MapType.ToString().ToLowerInvariant();
 
 			pictureBox.LoadAsync(AddKey(String.Format(CultureInfo.InvariantCulture,
-													  "http://maps.google.com/staticmap?size={0}x{1}&zoom={2}&maptype={3}&markers={4},{5},blue",
-													  640, 640, Zoom, mapTypeString, latitude, longitude)).ToString());
+													  "http://maps.google.com/maps/api/staticmap?size={0}x{1}&zoom={2}&maptype={3}&markers=color:blue|{4}&sensor=false",
+													  640, 640, Zoom, mapTypeString, Uri.EscapeDataString(AddressString))).ToString());
 		}
 		[SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Error message")]
 		private void pictureBox_Click(object sender, EventArgs e) {
@@ -171,7 +142,13 @@ namespace ShomreiTorah.WinForms.Controls {
 		}
 
 		private void pictureBox_LoadCompleted(object sender, AsyncCompletedEventArgs e) {
-			if (e.Error != null)
+			var wex = e.Error as WebException;
+			if (wex != null && wex.Response != null) {
+				using (var stream = wex.Response.GetResponseStream())
+				using (var reader = new StreamReader(stream)) {
+					SetError(reader.ReadToEnd());
+				}
+			} else if (e.Error != null)
 				SetError(e.Error.Message);
 		}
 	}
