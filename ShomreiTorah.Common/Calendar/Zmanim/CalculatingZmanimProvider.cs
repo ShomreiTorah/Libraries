01914477e617dfef33922e32680648a4f6ed9bc8
Schabse Laks <Schabse@GMail.com>
2009-12-27 11:53:01 -0500

    Working
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 1547]

--- ShomreiTorah.Common/Calendar/Zmanim/CalculatingZmanimProvider.cs
+++ ShomreiTorah.Common/Calendar/Zmanim/CalculatingZmanimProvider.cs
@@ -68,8 +68,8 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 			//todaysZmanim.plagMincha1 = convertToTime(todaysZmanim.mySunrise + (todaysZmanim.shaosZmanios1 * 10.75));
 			//todaysZmanim.plagMincha2 = convertToTime(todaysZmanim.alosHashachar1 + (todaysZmanim.shaosZmanios2 * 10.75));
 
-			retVal.Add(Zman.Sunrise, CalcSunTime(date, 545 / 6, true));
-			retVal.Add(Zman.Sunset, CalcSunTime(date, 545 / 6, false));
+			retVal.Add(Zman.Sunrise, CalcSunTime(date, 545 / 6.0, true));
+			retVal.Add(Zman.Sunset, CalcSunTime(date, 545 / 6.0, false));
 
 			var hour1 = (retVal[Zman.Sunset] - retVal[Zman.Sunrise]).Ticks / 12;
 			var hour2 = (TimeSpan.FromHours(2.4) + retVal[Zman.Sunset] - retVal[Zman.Sunrise]).Ticks / 12;
@@ -77,7 +77,7 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 			retVal.Add(Zman.עלות, retVal[Zman.Sunrise] - TimeSpan.FromHours(1.2));
 
 			retVal.Add(Zman.סוף٠זמן٠קריאת٠שמע٠גרא, retVal[Zman.Sunrise] + TimeSpan.FromTicks(hour1 * 3));
-			retVal.Add(Zman.סוף٠זמן٠קריאת٠שמע٠מ٠א, retVal[Zman.Sunrise] + TimeSpan.FromTicks(hour2 * 3));
+			retVal.Add(Zman.סוף٠זמן٠קריאת٠שמע٠מ٠א, retVal[Zman.עלות] + TimeSpan.FromTicks(hour2 * 3));
 
 			retVal.Add(Zman.חצות, retVal[Zman.Sunrise] + TimeSpan.FromTicks(hour1 * 6));
 
@@ -90,7 +90,7 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 			zenith = Cos(zenith);
 			var N1 = Math.Floor(275 * date.Month / 9.0);
 			var N2 = Math.Floor((date.Month + 9) / 12.0);
-			var N3 = (1 + Math.Floor((date.Year - 4 * Math.Floor(date.Year / 4.0) + 2) / 3));
+			var N3 = (1 + Math.Floor((date.Year - 4 * Math.Floor(date.Year / 4.0) + 2) / 3.0));
 			var N = N1 - (N2 * N3) + date.Day - 30;
 			var lngHour = Longitude / 15;
 
@@ -106,10 +106,10 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 				L -= 360;
 			}
 			var RA = Atan(.91746 * Tan(L));
-			var lQuadrant = (Math.Floor(L / 90)) * 90;
-			var RAQuadrant = (Math.Floor(RA / 90)) * 90;
-			RA = RA + (lQuadrant - RAQuadrant);
-			RA = RA / 15;
+			var lQuadrant = (Math.Floor(L / 90.0)) * 90;
+			var RAQuadrant = (Math.Floor(RA / 90.0)) * 90;
+			RA += (lQuadrant - RAQuadrant);
+			RA /= 15.0;
 			var sinDec = .39782 * Sin(L);
 			var cosDec = Cos(Asin(sinDec));
 			var cosH = (zenith - (sinDec * Sin(Latitude))) / (cosDec * Cos(Latitude));
@@ -117,7 +117,7 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 
 			if (sunrise)
 				H = 360 - H;
-			H /= 15;
+			H /= 15.0;
 
 			var T = H + RA - (.06571 * t) - 6.622;
 			var UT = T - lngHour;
@@ -130,9 +130,9 @@ namespace ShomreiTorah.Common.Calendar.Zmanim {
 		static double Cos(double degrees) { return Math.Cos(degrees * Math.PI / 180); }
 		static double Tan(double degrees) { return Math.Tan(degrees * Math.PI / 180); }
 
-		static double Asin(double number) { return Math.Asin(number) / Math.PI / 180; }
-		static double Acos(double number) { return Math.Acos(number) / Math.PI / 180; }
-		static double Atan(double number) { return Math.Atan(number) / Math.PI / 180; }
+		static double Asin(double number) { return Math.Asin(number) / (Math.PI / 180); }
+		static double Acos(double number) { return Math.Acos(number) / (Math.PI / 180); }
+		static double Atan(double number) { return Math.Atan(number) / (Math.PI / 180); }
 
 		///<summary>Gets the first date that this instance can provide Zmanim for.</summary>
 		public override DateTime MinDate { get { return DateTime.MinValue; } }
