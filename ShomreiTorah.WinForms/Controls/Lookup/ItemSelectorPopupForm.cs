25ac329d8aa6bfd2763a31a6f25e1269f57e0d7e
Schabse Laks <Schabse@GMail.com>
2010-09-16 12:59:51 -0500

    Disable unused functionality
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2443]

--- ShomreiTorah.WinForms/Controls/Lookup/ItemSelectorPopupForm.cs
+++ ShomreiTorah.WinForms/Controls/Lookup/ItemSelectorPopupForm.cs
@@ -369,7 +369,6 @@ namespace ShomreiTorah.WinForms.Controls.Lookup {
 		#region HSL
 		class HSL {
 			public HSL(double h, double s, double l) { H = h; S = s; L = l; }
-			public HSL(Color c) : this(c.GetHue() / 360.0, c.GetSaturation(), c.GetBrightness()) { }
 
 			double _h;
 			double _s;
@@ -395,7 +394,52 @@ namespace ShomreiTorah.WinForms.Controls.Lookup {
 				if (val > 1) return 1;
 				return val;
 			}
+			/// <summary>
+			/// Converts a color from HSL to RGB
+			/// </summary>
+			/// <remarks>Adapted from the algorithm in Foley and Van-Dam</remarks>
+			/// <returns>A Color structure containing the equivalent RGB values</returns>
+			public Color GetColor() {
+				double r = 0, g = 0, b = 0;
+				double temp1, temp2;
 
+				if (L == 0) {
+					r = g = b = 0;
+				} else {
+					if (S == 0) {
+						r = g = b = L;
+					} else {
+						temp2 = ((L <= 0.5) ? L * (1.0 + S) : L + S - (L * S));
+						temp1 = 2.0 * L - temp2;
+
+						double[] t3 = new double[] { H + 1.0 / 3.0, H, H - 1.0 / 3.0 };
+						double[] clr = new double[] { 0, 0, 0 };
+						for (int i = 0; i < 3; i++) {
+							if (t3[i] < 0)
+								t3[i] += 1.0;
+							if (t3[i] > 1)
+								t3[i] -= 1.0;
+
+							if (6.0 * t3[i] < 1.0)
+								clr[i] = temp1 + (temp2 - temp1) * t3[i] * 6.0;
+							else if (2.0 * t3[i] < 1.0)
+								clr[i] = temp2;
+							else if (3.0 * t3[i] < 2.0)
+								clr[i] = (temp1 + (temp2 - temp1) * ((2.0 / 3.0) - t3[i]) * 6.0);
+							else
+								clr[i] = temp1;
+						}
+						r = clr[0];
+						g = clr[1];
+						b = clr[2];
+					}
+				}
+
+				return Color.FromArgb((int)(255 * r), (int)(255 * g), (int)(255 * b));
+			}
+
+#if Unused //Unused additional functionality
+			public HSL(Color c) : this(c.GetHue() / 360.0, c.GetSaturation(), c.GetBrightness()) { }
 			/// <summary>
 			/// Sets the absolute brightness of a color
 			/// </summary>
@@ -479,50 +523,7 @@ namespace ShomreiTorah.WinForms.Controls.Lookup {
 				return hsl.GetColor();
 			}
 
-			/// <summary>
-			/// Converts a color from HSL to RGB
-			/// </summary>
-			/// <remarks>Adapted from the algorithm in Foley and Van-Dam</remarks>
-			/// <returns>A Color structure containing the equivalent RGB values</returns>
-			public Color GetColor() {
-				double r = 0, g = 0, b = 0;
-				double temp1, temp2;
-
-				if (L == 0) {
-					r = g = b = 0;
-				} else {
-					if (S == 0) {
-						r = g = b = L;
-					} else {
-						temp2 = ((L <= 0.5) ? L * (1.0 + S) : L + S - (L * S));
-						temp1 = 2.0 * L - temp2;
-
-						double[] t3 = new double[] { H + 1.0 / 3.0, H, H - 1.0 / 3.0 };
-						double[] clr = new double[] { 0, 0, 0 };
-						for (int i = 0; i < 3; i++) {
-							if (t3[i] < 0)
-								t3[i] += 1.0;
-							if (t3[i] > 1)
-								t3[i] -= 1.0;
-
-							if (6.0 * t3[i] < 1.0)
-								clr[i] = temp1 + (temp2 - temp1) * t3[i] * 6.0;
-							else if (2.0 * t3[i] < 1.0)
-								clr[i] = temp2;
-							else if (3.0 * t3[i] < 2.0)
-								clr[i] = (temp1 + (temp2 - temp1) * ((2.0 / 3.0) - t3[i]) * 6.0);
-							else
-								clr[i] = temp1;
-						}
-						r = clr[0];
-						g = clr[1];
-						b = clr[2];
-					}
-				}
-
-				return Color.FromArgb((int)(255 * r), (int)(255 * g), (int)(255 * b));
-
-			}
+#endif
 		}
 		#endregion
 		#endregion
