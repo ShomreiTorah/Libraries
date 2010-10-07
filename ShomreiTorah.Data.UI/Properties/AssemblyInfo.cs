e7ea9038ac7e387b1ba6aa2d8d83f719f94d0255
Schabse Laks <Schabse@GMail.com>
2010-10-07 11:03:28 -0500

    Data.UI (empty)
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2506]

---
+++ ShomreiTorah.Data.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShomreiTorah.Data.UI")]
+[assembly: AssemblyDescription("Shomrei Torah UI Framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shomrei Torah")]
+[assembly: AssemblyProduct("ShomreiTorah.Data.UI")]
+[assembly: AssemblyCopyright("Copyright © Shomrei Torah 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ec0a4d4-fd24-48c4-8285-8581a5437619")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
