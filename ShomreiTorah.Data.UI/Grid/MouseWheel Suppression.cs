37295e96948a755fb919e8b3e67ad954b706b002
Schabse Laks <Schabse@GMail.com>
2010-10-13 10:20:46 -0500

    Separate MouseWheel suppression
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2541]

---
+++ ShomreiTorah.Data.UI/Grid/MouseWheel Suppression.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.Utils.Serializing;
+using DevExpress.XtraEditors;
+
+namespace ShomreiTorah.Data.UI.Grid {
+	partial class SmartGridView {
+		#region MouseWheel suppression
+		bool allowEditorWheel;
+		///<summary>Gets or sets whether the view mouse wheel events will be processed by the active editor.</summary>
+		[Description("Gets or sets whether the view mouse wheel events will be processed by the active editor.")]
+		[Category("Behavior")]
+		[DefaultValue(false)]
+		[XtraSerializableProperty]
+		public bool AllowEditorMouseWheel {
+			get { return allowEditorWheel; }
+			set { allowEditorWheel = value; }
+		}
+
+		protected override void UpdateEditor(DevExpress.XtraEditors.Repository.RepositoryItem ritem, DevExpress.XtraEditors.Container.UpdateEditorInfoArgs args) {
+			base.UpdateEditor(ritem, args);
+			if (ActiveEditor != null) {
+				ActiveEditor.MouseWheel += ActiveEditor_MouseWheel;
+				var te = ActiveEditor as TextEdit;
+				if (te != null)
+					te.MaskBox.MouseWheel += ActiveEditor_MouseWheel;
+			}
+		}
+		///<summary>Hides the currently active editor, discarding any changes.</summary>
+		public override void HideEditor() {
+			base.HideEditor();
+			if (ActiveEditor != null) {
+				ActiveEditor.MouseWheel -= ActiveEditor_MouseWheel;
+				var te = ActiveEditor as TextEdit;
+				if (te != null)
+					te.MaskBox.MouseWheel += ActiveEditor_MouseWheel;
+			}
+		}
+
+		void ActiveEditor_MouseWheel(object sender, MouseEventArgs e) {
+			if (!AllowEditorMouseWheel) {
+				DXMouseEventArgs ee = DXMouseEventArgs.GetMouseArgs(e);
+				this.Handler.ProcessMouseWheel(ee);
+				ee.Handled = true;
+			}
+		}
+
+		partial class MyHandler {
+			internal void ProcessMouseWheel(DXMouseEventArgs ee) { OnMouseWheel(ee); }
+			protected override bool OnMouseWheel(MouseEventArgs e) {
+				DXMouseEventArgs ee = DXMouseEventArgs.GetMouseArgs(e);
+				//Copied from GridHandler source.
+				//Originally called base.OnMouseWheel, which called View.RaiseMouseWheel.
+				View.RaiseMouseWheel(ee);
+
+				if (ee.Handled) return true;
+				try {
+					if (View.AllowEditorMouseWheel && View.IsEditing) return false;
+					if (View.FilterPopup != null) return true;
+					View.TopRowIndex += (e.Delta > 0 ? -GetScrollLinesCount() : GetScrollLinesCount());
+					return true;
+				} finally {
+					if (View != null && View.ScrollInfo != null && View.ScrollInfo.VScroll != null && View.ScrollInfo.VScroll.Visible) ee.Handled = true;
+				}
+			}
+			int GetScrollLinesCount() {
+				return SystemInformation.MouseWheelScrollLines == -1 ? View.ScrollPageSize : SystemInformation.MouseWheelScrollLines;
+			}
+		}
+		#endregion
+	}
+}
--- ShomreiTorah.Data.UI/Grid/SmartGridView.cs
+++ ShomreiTorah.Data.UI/Grid/SmartGridView.cs
@@ -1,24 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Windows.Forms;
-using DevExpress.Utils;
+using DevExpress.Utils.Serializing;
 using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Columns;
 using DevExpress.XtraGrid.Registrator;
 using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraGrid.Views.Base.Handler;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid.Views.Grid.Handler;
-using DevExpress.Utils.Serializing;
-using DevExpress.XtraGrid.Views.Base.Handler;
-using DevExpress.XtraEditors;
-using System.Diagnostics;
 
 namespace ShomreiTorah.Data.UI.Grid {
 	///<summary>A grid view that automatically reads column settings from metadata.</summary>
-	public sealed class SmartGridView : GridView {
+	public  sealed partial class SmartGridView : GridView {
 		sealed class MyRegistrator : GridInfoRegistrator {
 			public override string ViewName { get { return "SmartGridView"; } }
 			public override BaseViewHandler CreateHandler(BaseView view) { return new MyHandler((SmartGridView)view); }
@@ -82,76 +78,14 @@ namespace ShomreiTorah.Data.UI.Grid {
 			}
 		}
 
-		#region MouseWheel suppression
-		bool allowEditorWheel;
-		///<summary>Gets or sets whether the view mouse wheel events will be processed by the active editor.</summary>
-		[Description("Gets or sets whether the view mouse wheel events will be processed by the active editor.")]
-		[Category("Behavior")]
-		[DefaultValue(false)]
-		[XtraSerializableProperty]
-		public bool AllowEditorMouseWheel {
-			get { return allowEditorWheel; }
-			set { allowEditorWheel = value; }
-		}
-
-		protected override void UpdateEditor(DevExpress.XtraEditors.Repository.RepositoryItem ritem, DevExpress.XtraEditors.Container.UpdateEditorInfoArgs args) {
-			base.UpdateEditor(ritem, args);
-			if (ActiveEditor != null) {
-				ActiveEditor.MouseWheel += ActiveEditor_MouseWheel;
-				var te = ActiveEditor as TextEdit;
-				if (te != null)
-					te.MaskBox.MouseWheel += ActiveEditor_MouseWheel;
-			}
-		}
-		///<summary>Hides the currently active editor, discarding any changes.</summary>
-		public override void HideEditor() {
-			base.HideEditor();
-			if (ActiveEditor != null) {
-				ActiveEditor.MouseWheel -= ActiveEditor_MouseWheel;
-				var te = ActiveEditor as TextEdit;
-				if (te != null)
-					te.MaskBox.MouseWheel += ActiveEditor_MouseWheel;
-			}
-		}
-
-		void ActiveEditor_MouseWheel(object sender, MouseEventArgs e) {
-			if (!AllowEditorMouseWheel) {
-				DXMouseEventArgs ee = DXMouseEventArgs.GetMouseArgs(e);
-				this.Handler.ProcessMouseWheel(ee);
-				ee.Handled = true;
-			}
-		}
-		#endregion
-
 		protected override void PopulateColumnsCore(DevExpress.Data.DataColumnInfo[] columns) {
 			base.PopulateColumnsCore(columns);
 		}
 
-		sealed class MyHandler : GridHandler {
+		sealed partial class MyHandler : GridHandler {
 			public MyHandler(SmartGridView view) : base(view) { }
 
 			public new SmartGridView View { get { return (SmartGridView)base.View; } }
-
-			internal void ProcessMouseWheel(DXMouseEventArgs ee) { OnMouseWheel(ee); }
-			protected override bool OnMouseWheel(MouseEventArgs e) {
-				DXMouseEventArgs ee = DXMouseEventArgs.GetMouseArgs(e);
-				//Copied from GridHandler source.
-				//Originally called base.OnMouseWheel, which called View.RaiseMouseWheel.
-				View.RaiseMouseWheel(ee);
-
-				if (ee.Handled) return true;
-				try {
-					if (View.AllowEditorMouseWheel && View.IsEditing) return false;
-					if (View.FilterPopup != null) return true;
-					View.TopRowIndex += (e.Delta > 0 ? -GetScrollLinesCount() : GetScrollLinesCount());
-					return true;
-				} finally {
-					if (View != null && View.ScrollInfo != null && View.ScrollInfo.VScroll != null && View.ScrollInfo.VScroll.Visible) ee.Handled = true;
-				}
-			}
-			int GetScrollLinesCount() {
-				return SystemInformation.MouseWheelScrollLines == -1 ? View.ScrollPageSize : SystemInformation.MouseWheelScrollLines;
-			}
 		}
 	}
 	///<summary>Contains the columns in a SmartGridView.</summary>
