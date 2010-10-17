740214a0ba507e48af43171bd24f6c8ef2154075
Schabse Laks <Schabse@GMail.com>
2010-10-16 21:00:20 -0500

    Initial registrations
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2563]

--- ShomreiTorah.Data.UI/DisplaySettings/SettingsRegistrator.cs
+++ ShomreiTorah.Data.UI/DisplaySettings/SettingsRegistrator.cs
@@ -2,14 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ShomreiTorah.Data.UI.Grid;
+using DevExpress.Data;
 
 namespace ShomreiTorah.Data.UI.DisplaySettings {
 	///<summary>Registers grid and column behaviors.</summary>
 	static class SettingsRegistrator {
 		static void InitializeStandardSettings() {
 			//This method will only be called once
+			EditorRepository.Register(new[] { Payment.AmountColumn, Pledge.AmountColumn }, EditorRepository.CurrencyEditor);
+			EditorRepository.Register(new[] { Payment.AccountColumn, Pledge.AccountColumn }, EditorRepository.AccountEditor);
 
+			GridManager.RegisterColumn(
+				Pledge.AmountColumn,
+				new ColumnController(c => {
+					c.DisplayFormat.Assign(c.DefaultEditor.DisplayFormat);
 
+					c.MaxWidth = 70;
+					c.SummaryItem.DisplayFormat = "{0:c} Total Pledged";
+					c.SummaryItem.SummaryType = SummaryItemType.Sum;
+				})
+			);
+			GridManager.RegisterColumn(
+				Payment.AmountColumn,
+				new ColumnController(c => {
+					c.DisplayFormat.Assign(c.DefaultEditor.DisplayFormat);
+
+					c.MaxWidth = 70;
+					c.SummaryItem.DisplayFormat = "{0:c} Total Paid";
+					c.SummaryItem.SummaryType = SummaryItemType.Sum;
+				})
+			);
+
+			GridManager.RegisterColumns(
+				new[] { Payment.AccountColumn, Pledge.AccountColumn },
+				new ColumnController(c => { c.MaxWidth = 100; })
+			);
 		}
 
 		static SettingsRegistrator() { InitializeStandardSettings(); }
