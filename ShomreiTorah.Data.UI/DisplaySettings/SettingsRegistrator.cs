c7a908e42b5370db7b9aa8b3d54dd8195c8c358e
Schabse Laks <Schabse@GMail.com>
2010-10-17 12:47:55 -0500

    Lots of behavior
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2583]

--- ShomreiTorah.Data.UI/DisplaySettings/SettingsRegistrator.cs
+++ ShomreiTorah.Data.UI/DisplaySettings/SettingsRegistrator.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Text;
 using ShomreiTorah.Data.UI.Grid;
 using DevExpress.Data;
+using ShomreiTorah.Singularity;
+using DevExpress.Utils;
+using DevExpress.XtraGrid.Views.Grid;
 
 namespace ShomreiTorah.Data.UI.DisplaySettings {
 	///<summary>Registers grid and column behaviors.</summary>
 	static class SettingsRegistrator {
 		static void InitializeStandardSettings() {
 			//This method will only be called once
-			EditorRepository.Register(new[] { Payment.AmountColumn, Pledge.AmountColumn }, EditorRepository.CurrencyEditor);
-			EditorRepository.Register(new[] { Payment.AccountColumn, Pledge.AccountColumn }, EditorRepository.AccountEditor);
+			EditorRepository.Register(new[] { Pledge.AmountColumn, Payment.AmountColumn }, EditorRepository.CurrencyEditor);
+			EditorRepository.Register(new[] { Pledge.AccountColumn, Payment.AccountColumn }, EditorRepository.AccountEditor);
+
+			GridManager.SuppressColumns(Pledge.ExternalSourceColumn, Pledge.ExternalIdColumn);
+			GridManager.SuppressColumns(Payment.ExternalSourceColumn, Payment.ExternalIdColumn);
+
+			GridManager.SuppressColumn(c => c.ColumnType == typeof(Guid));
+			GridManager.SuppressColumn(c => {
+				if (c.View.ParentView == null) return false;
+
+				var parentSchema = TableSchema.GetSchema(c.View.ParentView.DataSource);
+				var childSchema = TableSchema.GetSchema(c.View.DataSource);
+
+				//If this column is a foreign key that references the schema of the parent view, hide it.
+				if (parentSchema == null || childSchema == null)
+					return false;
+				var fkc = childSchema.Columns[c.FieldName] as ForeignKeyColumn;
+
+				return fkc != null && fkc.ForeignSchema == parentSchema;
+			});
+
+			GridManager.RegisterBehavior(
+				new TableSchema[] { Pledge.Schema, Payment.Schema },
+				new AdvancedColumnsBehavior("modifier columns", new[] { "Modified", "Modifier" })
+			);
 
 			GridManager.RegisterColumn(
 				Pledge.AmountColumn,
@@ -39,8 +65,11 @@ namespace ShomreiTorah.Data.UI.DisplaySettings {
 				new ColumnController(c => { c.MaxWidth = 100; })
 			);
 
-			GridManager.RegisterColumns(
-				new[] { Pledge.PersonColumn, Payment.PersonColumn, EmailAddress.PersonColumn },
+			GridManager.RegisterColumn(
+				c => {
+					var fkc = c as ForeignKeyColumn;
+					return fkc != null && fkc.ForeignSchema == Person.Schema;
+				},
 				new PersonColumnController()
 			);
 			GridManager.RegisterColumn(Payment.DepositColumn, new DepositColumnController());
@@ -48,26 +77,44 @@ namespace ShomreiTorah.Data.UI.DisplaySettings {
 
 		class PersonColumnController : ColumnController {
 			protected internal override void Apply(SmartGridColumn column) {
+				column.OptionsColumn.ReadOnly = true;
+				column.OptionsColumn.AllowSort = DefaultBoolean.True;
+				column.OptionsColumn.AllowGroup = DefaultBoolean.True;
+
 				column.Caption = "Full Name";
 				column.SortIndex = 0;
 				column.SortOrder = ColumnSortOrder.Ascending;
-				column.OptionsColumn.ReadOnly = true;
 			}
 			protected internal override string GetDisplayText(object row, object value) {
 				var person = (Person)value;
+				if (person == null) return "";
 				return person.FullName;
 			}
 		}
 		class DepositColumnController : ColumnController {
 			protected internal override void Apply(SmartGridColumn column) {
+				column.OptionsColumn.AllowSort = DefaultBoolean.True;
+				column.OptionsColumn.AllowGroup = DefaultBoolean.True;
+
 				column.OptionsColumn.AllowEdit = false;
 				column.OptionsColumn.ReadOnly = true;
 			}
 			protected internal override string GetDisplayText(object row, object value) {
 				var deposit = (Deposit)value;
-				if (deposit == null) 
+				if (deposit == null)
 					return "Undeposited";
 				return deposit.Date.ToShortDateString() + " #" + deposit.Number;
+			}
+			protected internal override void OnShowFilterPopupListBox(FilterPopupListBoxEventArgs e) {
+				foreach (var item in e.ComboBox.Items.OfType<FilterItem>()) {
+					var valueItem = item.Value as FilterItem;
+					if (valueItem != null) {
+						if ((int)valueItem.Value == 2)
+							item.Text = "(Undeposited)";
+						else if ((int)valueItem.Value == 3)
+							item.Text = "(All deposited)";
+					}
+				}
 			}
 		}
 
