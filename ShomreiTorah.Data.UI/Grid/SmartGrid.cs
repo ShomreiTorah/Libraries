00afc4557796f5e29cd93f64ba74907e3631ceb4
Schabse Laks <Schabse@GMail.com>
2010-10-16 21:52:39 -0500

    Best-fit (subpar)
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2566]

--- ShomreiTorah.Data.UI/Grid/SmartGrid.cs
+++ ShomreiTorah.Data.UI/Grid/SmartGrid.cs
@@ -7,6 +7,7 @@ using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Registrator;
 using DevExpress.XtraGrid.Views.Base;
 using ShomreiTorah.Common;
+using DevExpress.XtraGrid.Views.Grid;
 
 namespace ShomreiTorah.Data.UI.Grid {
 	///<summary>A grid control that automatically reads column settings from metadata.</summary>
@@ -28,6 +29,21 @@ namespace ShomreiTorah.Data.UI.Grid {
 		protected internal new BaseView MouseCaptureOwner {
 			get { return base.MouseCaptureOwner; }
 			set { base.MouseCaptureOwner = value; }
+		}
+
+		protected override void OnHandleCreated(EventArgs e) {
+			base.OnHandleCreated(e);
+			if (!DesignMode) {
+				var gridView = MainView as GridView;
+				if (gridView != null)
+					gridView.BestFitColumns();	//TODO: Move to SmartGridView
+			}
+		}
+		protected override void RegisterView(BaseView gv) {
+			base.RegisterView(gv);
+			var view = gv as GridView;
+			if (view != null)
+				view.BestFitColumns();	//For detail clones
 		}
 	}
 }
