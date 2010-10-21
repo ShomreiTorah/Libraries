728d0fd13a971d716b5c0c531a34df5d1f7e417b
Schabse Laks <Schabse@GMail.com>
2010-10-21 09:52:52 -0500

    Tooltip tweaks
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2621]

--- ShomreiTorah.Data.UI/Extensions.cs
+++ ShomreiTorah.Data.UI/Extensions.cs
@@ -13,16 +13,20 @@ namespace ShomreiTorah.Data.UI {
 		///<summary>Creates a SuperToolTip that displays information about a person.</summary>
 		public static SuperToolTip GetSuperTip(this Person person) {
 			if (person == null) throw new ArgumentNullException("person");
-			return Utilities.CreateSuperTip(person.VeryFullName, person.MailingAddress);
+
+			string body = person.MailingAddress;
+			if (!string.IsNullOrEmpty(person.Phone))
+				body += Environment.NewLine + Environment.NewLine + person.Phone;
+
+			return Utilities.CreateSuperTip(person.VeryFullName, body);
 		}
 
 		///<summary>Creates a SuperToolTip that displays information about a deposit.</summary>
 		public static SuperToolTip GetSuperTip(this Deposit deposit) {
 			if (deposit == null) throw new ArgumentNullException("deposit");
 			return Utilities.CreateSuperTip(
-				"Deposit",
-				String.Format(CultureInfo.CurrentCulture, "Deposit #{0} on {1:F}; deposit contained {2:c}.",
-														  deposit.Number, deposit.Date, deposit.Payments.Sum(p => p.Amount))
+				body: String.Format(CultureInfo.CurrentCulture, "Deposit #{0} on {1:D}.\r\nDeposit contained {2:c} in {3} payments.",
+																deposit.Number, deposit.Date, deposit.Payments.Sum(p => p.Amount), deposit.Payments.Count)
 			);
 		}
 		#endregion
