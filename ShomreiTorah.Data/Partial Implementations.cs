372735e1bff622249987965e5fec8ff7e9cbe142
Schabse Laks <Schabse@GMail.com>
2010-10-25 10:18:07 -0500

    Empty deposit cleanup
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2664]

--- ShomreiTorah.Data/Partial Implementations.cs
+++ ShomreiTorah.Data/Partial Implementations.cs
@@ -7,8 +7,18 @@ using System.Net.Mail;
 using System.Text;
 
 namespace ShomreiTorah.Data {
-	[SuppressMessage("Microsoft.Design", "CA1036:OverrideMethodsOnComparableTypes", Justification = "Comparable for grids only")]
-	partial class Person : IComparable<Person>, IComparable {
+	partial class Person {
+		partial void OnFullNameChanged(string oldValue, string newValue) {
+			if (Table == null || Table.Context == null)
+				return;
+			var emails = Table.Context.Table<EmailAddress>();
+			if (emails != null) return;
+			foreach (var email in emails.Rows) {
+				if (String.IsNullOrWhiteSpace(email.Name) || email.Name.Trim() == oldValue)
+					email.Name = newValue;
+			}
+		}
+
 		#region String properties
 		///<summary>Gets the family's full name, including both spouses.</summary>
 		public string VeryFullName {
@@ -45,18 +55,11 @@ namespace ShomreiTorah.Data {
 			}
 		}
 		#endregion
+	}
 
-		partial void OnFullNameChanged(string oldValue, string newValue) {
-			if (Table == null || Table.Context == null)
-				return;
-			var emails = Table.Context.Table<EmailAddress>();
-			if (emails != null) return;
-			foreach (var email in emails.Rows) {
-				if (String.IsNullOrWhiteSpace(email.Name) || email.Name.Trim() == oldValue)
-					email.Name = newValue;
-			}
-		}
-
+	#region Comparable
+	[SuppressMessage("Microsoft.Design", "CA1036:OverrideMethodsOnComparableTypes", Justification = "Comparable for grids only")]
+	partial class Person : IComparable<Person>, IComparable {
 		int IComparable.CompareTo(object obj) { return CompareTo(obj as Person); }
 		///<summary>Compares this person to another Person instance.</summary>
 		public int CompareTo(Person other) {
@@ -82,6 +85,29 @@ namespace ShomreiTorah.Data {
 			return result == 0 ? Number.CompareTo(other.Number) : result;
 		}
 	}
+	#endregion
+
+	#region Empty deposit cleanup
+	partial class Deposit {
+		///<summary>Called before the row is removed from its table.</summary>
+		protected override void OnRemoving() {
+			foreach (var payment in Payments.ToArray()) {	//The loop will modify the collection
+				payment.Deposit = null;
+			}
+		}
+	}
+	partial class Payment {
+		///<summary>Called before the row is removed from its table.</summary>
+		protected override void OnRemoving() {
+			if (Deposit != null && Deposit.Payments.Count == 1)
+				Deposit.RemoveRow();
+		}
+		partial void OnDepositChanged(Deposit oldValue, Deposit newValue) {
+			if (oldValue.Payments.Count == 0)
+				oldValue.RemoveRow();
+		}
+	}
+	#endregion
 
 	partial class EmailAddress {
 		partial void ValidateEmail(string newValue, Action<string> error) {
