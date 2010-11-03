ff5f3b22cce1c758a06fbc8f5c093ab8e9fcd642
Schabse Laks <Schabse@GMail.com>
2010-11-02 19:19:44 -0500

    Suppress EventArgss
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2841]

--- ShomreiTorah.Data.UI/DisplaySettings/RowDetailBehavior.cs
+++ ShomreiTorah.Data.UI/DisplaySettings/RowDetailBehavior.cs
@@ -1,19 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ShomreiTorah.Data.UI.Grid;
 using System.Windows.Forms;
+using DevExpress.Utils;
+using ShomreiTorah.Data.UI.Grid;
 using ShomreiTorah.Singularity;
+
 namespace ShomreiTorah.Data.UI.DisplaySettings {
 	///<summary>A grid behavior that shows row detail forms when rows are double-clicked.</summary>
 	class RowDetailBehavior : IGridBehavior {
 		public void Apply(SmartGridView view) {
-			view.DoubleClick += delegate {
+			view.DoubleClick += (sender, e) => {
 				var info = view.CalcHitInfo(view.GridControl.PointToClient(Control.MousePosition));
 
-				if (info.RowHandle >= 0 && info.InRow)
+				if (info.RowHandle >= 0 && info.InRow) {
 					AppFramework.Current.ShowDetails((Row)view.GetRow(info.RowHandle));
+
+					var dx = e as DXMouseEventArgs;
+					if (dx != null) dx.Handled = true;
+				}
 			};
 		}
 	}
