e0d9cd4a583b27bcf5bffa3c59d711dd9d9023b1
Schabse Laks <Schabse@GMail.com>
2010-11-18 21:59:26 -0500

    GetDependencies extensio
    [git-p4: depot-paths = "//depot/Shul/Libraries/": change = 2991]

--- ShomreiTorah.Singularity/Extensions.cs
+++ ShomreiTorah.Singularity/Extensions.cs
@@ -6,10 +6,10 @@ using System.Text;
 namespace ShomreiTorah.Singularity {
 	///<summary>Contains extension methods.</summary>
 	public static class Extensions {
-		///<summary>Sorts a set of schemas so that each schema is returned after all of its dependencies.</summary>
+		///<summary>Sorts a set of schemas so that each schema is returned after all of its dependencies (a topologic sort).</summary>
 		///<remarks>If the schemas contain a cyclic dependency, an exception will be thrown.</remarks>
 		public static IEnumerable<TableSchema> SortDependencies(this IEnumerable<TableSchema> items) { return items.SortDependencies(ts => ts); }
-		///<summary>Sorts a set of schemas so that each schema is returned after all of its dependencies.</summary>
+		///<summary>Sorts a set of schemas so that each schema is returned after all of its dependencies (a topologic sort).</summary>
 		///<remarks>If the schemas contain a cyclic dependency, an exception will be thrown.</remarks>
 		public static IEnumerable<T> SortDependencies<T>(this IEnumerable<T> items, Func<T, TableSchema> schemaSelector) {
 			if (items == null) throw new ArgumentNullException("items");
@@ -37,6 +37,17 @@ namespace ShomreiTorah.Singularity {
 				if (returnedTables.Count == 0)
 					throw new InvalidOperationException("Cyclic dependency detected");
 				pendingItems.RemoveAll(returnedTables.Contains);
+			}
+		}
+
+		///<summary>Gets all of the schemas referenced by foreign key columns in a schema.  The returned schemas will be topologically sorted.</summary>
+		///<remarks>If the schema involves a cyclic dependency, a  stack overflow will occur.</remarks>
+		public static IEnumerable<TableSchema> GetDependencies(this TableSchema schema) {
+			foreach (var fkc in schema.Columns.OfType<ForeignKeyColumn>()) {
+				foreach (var parent in fkc.ForeignSchema.GetDependencies())
+					yield return parent;
+
+				yield return fkc.ForeignSchema;		//First the parents, then the schema.
 			}
 		}
 	}
