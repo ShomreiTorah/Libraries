34acf6bca286e9c0bd4f1e9b2245032654b4787c
SLaks <Schabse@Gmail.com>
2012-06-22 14:16:20 -0400

    Add UpdateApplier for Common

---
+++ UpdateApplier/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace UpdateApplier {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args) {
+			try {
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+
+				var parentId = int.Parse(args[0], CultureInfo.InvariantCulture);
+				Process opener = Process.GetProcessById(parentId);
+
+				var sourceFolder = args[1];
+				var destFolder = args[2];
+				var restartCommand = GetCommandLine((uint)parentId);
+
+				//The parent process calls ReadLine, so it will wait for this line before quitting.
+				//Otherwise, it might quit before we start.  See UpdateChecker.ApplyUpdate.
+				Console.WriteLine("Please quit");
+				opener.WaitForExit();
+
+				FileSystem.MoveDirectory(sourceFolder, destFolder, true);
+
+				Launch(restartCommand + " Updated");
+			} catch (Exception ex) {
+				var message = new StringBuilder();
+				message.AppendLine("An error occurred while applying the update.\r\nPlease contact Schabse; you can press Ctrl+C to copy this message.\r\n");
+				message.AppendLine(ex.ToString());
+				foreach (DictionaryEntry kvp in ex.Data) {
+					var subEx = kvp.Value as Exception;
+
+					message.AppendLine().Append(kvp.Key).Append(": ").Append(subEx != null ? subEx.Message : kvp.Value.ToString());
+				}
+
+				MessageBox.Show(message.ToString(), "Shomrei Torah", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		static string GetCommandLine(uint id) {
+			using (var searcher = new ManagementObjectSearcher("SELECT CommandLine FROM WIN32_PROCESS where ProcessId=" + id.ToString(CultureInfo.InvariantCulture)))
+			using (var results = searcher.Get())
+				return (string)results.Cast<ManagementObject>().First()["CommandLine"];
+		}
+		static void Launch(string commandLine) {
+			string fileName, args;
+
+			int pathEnd;
+			if (commandLine[0] == '"') {		//For some reason, I decided to handle quoted paths with embedded (doubled) quotes, even at the beginning
+				pathEnd = -1;
+
+				do {
+					pathEnd++;
+					pathEnd = commandLine.IndexOf('"', pathEnd + 1);
+				} while (commandLine[pathEnd + 1] == '"');
+
+				fileName = commandLine.Substring(1, pathEnd - 1).Replace("\"\"", "\"");
+				args = commandLine.Substring(pathEnd + 1);
+			} else {
+				pathEnd = commandLine.IndexOf(' ');
+
+				fileName = commandLine.Remove(pathEnd);
+				args = commandLine.Substring(pathEnd + 1);
+			}
+
+			Process.Start(fileName, args);
+		}
+	}
+}
---
+++ UpdateApplier/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UpdateApplier")]
+[assembly: AssemblyDescription("Copies updates from a temp directory")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Congregation Shomrei Torah")]
+[assembly: AssemblyProduct("UpdateApplier")]
+[assembly: AssemblyCopyright("Copyright © Congregation Shomrei Torah 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a397e8e-3d98-424f-bab8-4712be761674")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
