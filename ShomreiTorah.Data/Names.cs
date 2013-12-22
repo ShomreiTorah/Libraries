1b612f10dfa8c85fca106767ca15f0d5502ed714
Schabse Laks <Dev@SLaks.net>
2013-12-22 14:49:58 -0500

    Journal price boost
    Also eliminate Greeting ads

--- ShomreiTorah.Data/Names.cs
+++ ShomreiTorah.Data/Names.cs
@@ -190,15 +190,14 @@ namespace ShomreiTorah.Data {
 		public override string ToString() { return DisplayAs; }
 		//Exposed by Names for consistency
 		internal readonly static ReadOnlyCollection<AdType> All = new ReadOnlyCollection<AdType>(new AdType[] { 
-			new AdType(1,	"Diamond",	1250,	1),
-			new AdType(2,	"Platinum",	1000,	1),
-			new AdType(3,	"Gold",		750,	1),
-			new AdType(4,	"Silver",	500,	1),
-			new AdType(5,	"Bronze",	360,	1),
-	 Full =	new AdType(6,	"Full",		250,	1),
+			new AdType(1,	"Diamond",	1800,	1),
+			new AdType(2,	"Platinum",	1250,	1),
+			new AdType(3,	"Gold",		1000,	1),
+			new AdType(4,	"Silver",	750,	1),
+			new AdType(5,	"Bronze",	500,	1),
+	 Full =	new AdType(6,	"Full",		360,	1),
 			new AdType(7,	"Half",		180,	2),
-			new AdType(8,	"Quarter",	100,	4),
-			new AdType(9,	"Greeting",	50,		10)
+			new AdType(8,	"Quarter",	100,	4)
 		});
 
 		static readonly AdType Full;
