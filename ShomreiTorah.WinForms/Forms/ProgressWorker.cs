5aef9e75c4bd5fd323fcffe0a28d574faf19503b
Schabse Laks <Dev@SLaks.net>
2016-11-14 22:46:19 -0500

    WinForms: Add ProgressWorker.ExecuteAsync()

--- ShomreiTorah.WinForms/Forms/ProgressWorker.cs
+++ ShomreiTorah.WinForms/Forms/ProgressWorker.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ShomreiTorah.Common;
 
@@ -13,13 +14,13 @@ namespace ShomreiTorah.WinForms.Forms {
 	public static class ProgressWorker {
 		///<summary>Executes an operation and displays its progress.</summary>
 		///<param name="method">The method to execute on the background thread.</param>
-		///<param name="cancellable">Indicates the visibilty of the cancel button.</param>
+		///<param name="cancellable">Indicates the visibility of the cancel button.</param>
 		///<returns>False if the cancel button was clicked.</returns>
 		public static bool Execute(Action<IProgressReporter> method, bool cancellable) { return Execute(null, method, cancellable); }
 		///<summary>Executes an operation and displays its progress.</summary>
 		///<param name="parent">The form that will own the progress display.  This parameter can be null.</param>
 		///<param name="method">The method to execute on the background thread.</param>
-		///<param name="cancellable">Indicates the visibilty of the cancel button.</param>
+		///<param name="cancellable">Indicates the visibility of the cancel button.</param>
 		///<returns>False if the cancel button was clicked.</returns>
 		[SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Cross-thread exception")]
 		public static bool Execute(IWin32Window parent, Action<IProgressReporter> method, bool cancellable) {
@@ -49,7 +50,34 @@ namespace ShomreiTorah.WinForms.Forms {
 				throw new TargetInvocationException(exception);
 			return !canceled;
 		}
+		///<summary>Executes an asynchronous operation and displays its progress.</summary>
+		///<param name="method">The method to execute.  This should not do any immediate blocking work.</param>
+		public static Task ExecuteAsync(Func<IProgressReporter, CancellationToken, Task> method) => ExecuteAsync(null, method);
+		///<summary>Executes a cancellable asynchronous operation and displays its progress.</summary>
+		///<param name="parent">The form that will own the progress display.  This parameter can be null.</param>
+		///<param name="method">The method to execute.  This should not do any immediate blocking work.</param>
+		///<returns>False if the cancel button was clicked.</returns>
+		public static async Task ExecuteAsync(IWin32Window parent, Func<IProgressReporter, CancellationToken, Task> method) {
+			if (method == null) throw new ArgumentNullException("method");
+
+			using (var dialog = new ProgressForm() { CanCancel = true }) {
+				dialog.FadeIn();
+				try {
+					var task = method(dialog, dialog.CancellationSource.Token);
+					await Task.WhenAny(task, Task.Delay(5));    // If it finishes quickly, don't bother showing the dialog.
+					if (!task.IsCompleted)
+						dialog.ShowDialog(parent);
+					await task;
+				} finally {
+					dialog.Finished = true;
+					dialog.FadeOut();
+				}
+				if (dialog.WasCanceled)
+					throw new TaskCanceledException();
+			}
+		}
 		class ProgressForm : ProgressDialog, IProgressReporter {
+			public CancellationTokenSource CancellationSource { get; } = new CancellationTokenSource();
 			public bool Finished { get; set; }
 
 			protected override void OnFadedIn() {
@@ -82,6 +110,7 @@ namespace ShomreiTorah.WinForms.Forms {
 				base.OnCancelClicked(e);
 				WasCanceled = true;
 				CancelState = ButtonMode.Disabled;
+				CancellationSource.Cancel();
 			}
 			public bool WasCanceled { get; private set; }
 
